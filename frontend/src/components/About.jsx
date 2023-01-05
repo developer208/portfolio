36753ccea7ae054a9e3160f378f01e6185a02d40
developer208 <it.developer2002@gmail.com>
2023-01-05 14:50:21 +0530

    minor changes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,11 +4,13 @@ import {themeContext} from "../provider/ThemeProvider"
 const About = () => {
   const {dark}=useContext(themeContext);
   return (
-    <div className= {dark ?'min-h-[100vh]  pt-[60px] text-center text-white bg-[#003744]   w-full ':'h-[100vh] text-center pt-[60px]  w-full ' }>
+<div className="h-[100%]">
+<div className= {dark ?'min-h-[100vh]  pt-[60px] text-center text-white bg-[#003744]   w-full ':'h-[100vh] text-center pt-[60px]  w-full ' }>
 
-    About page
-    
-      </div>
+About page
+
+  </div>
+</div>
   );
 };
 
